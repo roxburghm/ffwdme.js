fbd1a561b91d0fc43e217ad8784ee76048e00a60
matt <matt@roxburghm.com>
2017-12-05 14:45:41 +0000

    new FLEX layout and swap +/- to be -/+

--- src/components/zoom/index.js
+++ src/components/zoom/index.js
@@ -7,6 +7,8 @@ var Zoom = Base.extend({
     this.render();
   },
 
+  classname: "Zoom",
+
   attrAccessible: ['map', 'grid'],
 
   iconElZoomIn: null,
@@ -17,7 +19,7 @@ var Zoom = Base.extend({
 
   iconZoomOut: 'zoom/minus.svg',
 
-  classes: 'ffwdme-components-container ffwdme-components-zoom-container ffwdme-grid-w3 ffwdme-grid-h1',
+  classes: 'ffwdme-components-container ffwdme-components-zoom-container ffwdme-grid-w3 ffwdme-grid-h1 ffwdme-clickable',
 
   imgUrl: function(icon){
     return this.getRetinaImageUrl(ffwdme.defaults.imageBaseUrl + icon);
@@ -31,17 +33,18 @@ var Zoom = Base.extend({
   setIcons: function() {
     var img;
 
-    if (!this.iconElZoomIn) {
-      img = document.createElement('img');
-      this.iconElZoomIn = $(img).addClass('ffwdme-components-zoom').appendTo($(this.el));
-    }
-    this.iconElZoomIn[0].src = this.imgUrl(this.iconZoomIn);
-
     if (!this.iconElZoomOut) {
       img = document.createElement('img');
       this.iconElZoomOut = $(img).addClass('ffwdme-components-zoom').appendTo($(this.el));
     }
     this.iconElZoomOut[0].src = this.imgUrl(this.iconZoomOut);
+
+      if (!this.iconElZoomIn) {
+          img = document.createElement('img');
+          this.iconElZoomIn = $(img).addClass('ffwdme-components-zoom').appendTo($(this.el));
+      }
+      this.iconElZoomIn[0].src = this.imgUrl(this.iconZoomIn);
+
   },
 
   make: function(){
